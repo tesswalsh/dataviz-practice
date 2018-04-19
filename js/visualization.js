82850a299177f41bda4b8622ef707c17dd791c27
unknown <walsh.the@gmail.com>
2018-04-19 14:04:47 -0400

    added circle, learned attributes

---
+++ js/visualization.js
@@ -0,0 +1,4 @@
+		d3.select("circle").transition().duration(500)
+			.style("opacity", 0.5)
+			.attr("r", "100")
+			.attr("cx","100")
---
+++ server.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Alex\Documents\DATAVIZ1
+browser-sync start --server --port 8888 --files="**/*"
